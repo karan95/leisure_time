5f898c2bdf4fb9bf3a1a56f01ba7e75403240772
karan95 <kmthakor95@gmail.com>
2017-04-06 21:48:19 +0530

    Jwt token for authentication

--- leisure_time_REST/model/user/userAuth.js
+++ leisure_time_REST/model/user/userAuth.js
@@ -1,3 +1,6 @@
+var uuid = require('uuid');
+var nJwt = require('njwt');
+
 exports.checkUserInfo = function(req, res) {
     db.collection('userInfo', function(err, collection) {
         let userData = req.body;
@@ -7,9 +10,25 @@ exports.checkUserInfo = function(req, res) {
             } else {
                 for (let i = 0; i < users.length; i++) {
                     if (users[i].userName == userData.userName && users[i].password == userData.password) {
-                        res.status(200).json(users[i]);
+                        var secretKey = uuid.v4();
+                        var claims = {
+                            sub: users[i].userName,
+                            iss: 'https://leisuretime.com',
+                            permissions: 'upload-review'
+                        }
+                        var jwt = nJwt.create(claims, secretKey);
+                        var token = jwt.compact();
+                        let userAuthResponse = {
+                            'id': users[i]._id,
+                            'userName': users[i].userName,
+                            'name': users[i].name,
+                            'birthDate': users[i].birthDate,
+                            'gender': users[i].gender,
+                            'token': token
+                        }
+                        res.status(200).json(userAuthResponse);
                     } else {
-                        res.status(300).json({ "error": "error" });
+                        res.status(400).json({ "error": "Username or password is incorrect" });
                     }
                 }
             }
