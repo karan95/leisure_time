3e52355a05d306fb1e847eae1bee9330f0040d06
karan95 <kmthakor95@gmail.com>
2017-05-03 22:28:31 +0530

    review bug fixed

--- src/app/lt-components/feeds-review-text.pipe.ts
+++ src/app/lt-components/feeds-review-text.pipe.ts
@@ -9,9 +9,21 @@ export class FeedsReviewTextPipe implements PipeTransform {
     if (value ) {
       // let limit = args.length > 0 ? parseInt(args[0], 50) : 50;
       let limit = 200;
-      // let trail = args.length > 1 ? args[1] : '...';
-      let trail = '...';
-      return value.length > limit ? value.substring(0, limit) + trail : value;
+      if (value.length > limit) {
+        // var span = document.createElement( 'span' );
+        var displayContent = document.createElement( 'p' );
+        var extraContent = document.createElement( 'p' );
+        // let trail = args.length > 1 ? args[1] : '...';
+        displayContent.innerText = value.substring(0, limit);
+        extraContent.innerText =  value.substring(limit, value.length);
+        // span.innerHTML = '<a>more</a>';
+        let trail = ' ...';
+        // return '<a click="">more</a><span>awrarawr</span>'
+        return value;
+      } else {
+        return value;
+      }
+      // return value.length > limit ? value.substring(0, limit) + trail + pel : value;
     } else {
       return '';
     }
