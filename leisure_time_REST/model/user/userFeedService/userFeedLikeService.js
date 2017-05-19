1e7cf3fa1dacca289f71d8083da1fb249606ccc6
karan95 <kmthakor95@gmail.com>
2017-05-19 19:40:47 +0530

    bug fixing

--- leisure_time_REST/model/user/userFeedService/userFeedLikeService.js
+++ leisure_time_REST/model/user/userFeedService/userFeedLikeService.js
@@ -4,36 +4,40 @@ exports.userFeedLike = function(req, res) {
     if (authenticateReq(req, res)) {
         db.collection('userFeeds', function(err, collection) {
             var userFeedLikeData = req.body;
+            userFeedLikeData.userId = req.query.uid;
             collection.findOne({ 'userId': req.query.uid }, function(err, items) {
                 if (err) {
                     res.send("There was a problem getting the information from the database.");
                 } else {
+                    var userFeedData = {}
                     for (var i in items.feeds) {
                         if (items.feeds[i].feedId == userFeedLikeData.feedId) {
-                            var userFeedData = items.feeds[i];
-                            collection.update({ 'feedId': items.feeds[i].feedId }, { $pull: { "feeds": userFeedData } }, function(err, doc) {
+                            userFeedData = items.feeds[i];
+                        }
+                    }
+                    collection.update({ 'userId': req.query.uid }, { $pull: { "feeds": userFeedData } }, function(err, doc) {
+                        if (err) {
+                            // If it failed, return error
+                            res.send("There was a problem updating the information into the database.");
+                        } else {
+                            for (var i in userFeedData.likes) {
+                                if (userFeedLikeData.userId == userFeedData.likes[i].userId) {
+                                    userFeedData.likes.splice(i, 1);
+                                }
+                            }
+                            userFeedData.likes.push(userFeedLikeData);
+                            collection.update({ 'userId': req.query.uid }, { $push: { "feeds": userFeedData } }, function(err, doc) {
                                 if (err) {
                                     // If it failed, return error
                                     res.send("There was a problem adding the information to the database.");
                                 } else {
-                                    userFeedData.likes.push(userFeedLikeData);
-                                    console.log(userFeedData);
-                                    collection.update({ 'userId': req.query.uid }, { $push: { "feeds": userFeedData } }, function(err, doc) {
-                                        if (err) {
-                                            // If it failed, return error
-                                            res.send("There was a problem adding the information to the database.");
-                                        } else {
-                                            // res.status(201).json({ 'success': 'success' });
-                                            // res.redirect("/feeds");
-                                        }
-                                    });
+                                    res.status(201).json({ 'success': 'success' });
                                 }
                             });
                         }
-                    }
+                    });
                 }
             });
-            res.status(201).json({ 'success': 'success' });
         });
     } else {
         res.status(400).json({ 'error': 'There was problem while authenticating user.' });
