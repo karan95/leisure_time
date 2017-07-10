25802c38bd0bd6d14469435a202d9c5e6251d973
karan95 <kmthakor95@gmail.com>
2017-07-10 21:36:43 +0530

    friend list layout in user profile

--- src/app/lt-components/lt-components.module.ts
+++ src/app/lt-components/lt-components.module.ts
@@ -2,13 +2,11 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { AlertComponent } from './alert/alert.component';
-import { FeedsLoadingComponent } from './feeds-loading/feeds-loading.component';
 import { ParkingComponent } from './parking/parking.component';
 import { RatingComponent } from '../lt-components/rating/rating.component';
 
 const LT_COMPONENTS = [
   AlertComponent,
-  FeedsLoadingComponent,
   ParkingComponent,
   RatingComponent
 ];
