fcb7d4fc45248ea012021bdb6bdf173964eabb12
karan95 <kmthakor95@gmail.com>
2017-07-27 21:55:25 +0530

    validation changes in user login page

--- src/app/login/user-login/user-login.component.ts
+++ src/app/login/user-login/user-login.component.ts
@@ -1,12 +1,11 @@
 import { Component, OnInit } from '@angular/core';
-import { FormGroup, FormBuilder, Validators } from '@angular/forms';
+import { FormGroup, FormBuilder, Validators, FormControl } from '@angular/forms';
 import { Router, ActivatedRoute } from '@angular/router';
 import { MdSnackBar, MdSnackBarConfig } from '@angular/material';
 import { UserService } from '../../services/user/user.service';
 import { AuthenticationService } from '../../services/user/authentication.service';
 import { AlertService } from '../../lt-components/alert/alert.service';
 import { NotificationService } from '../../services/notification/notification.service';
-import { CustomValidators } from 'ng2-validation';
 
 @Component({
   selector: 'app-user-login',
@@ -27,19 +26,27 @@ export class UserLoginComponent implements OnInit {
         private _userService: UserService,
         private _alertService: AlertService,
         private _notificationService: NotificationService
-    ) {
+    ) {}
+
+    ngOnInit() {
+        this.buildForm();
+    }
+    
+    buildForm(): void {
         this.userLoginForm = this.formBuilder.group({
             'userName': ['', [Validators.required]],
             'password': ['', [Validators.required]]
         });
         this.userRegisterForm = this.formBuilder.group({
-            'name': ['', [Validators.required]],
-            'userName': ['', [Validators.required]],
-            'email': ['', [CustomValidators.email]],
-            'password': ['', [Validators.required]],
+            'name': ['', [Validators.required, Validators.minLength(4), Validators.maxLength(24)]],
+            'userName': ['', [Validators.required, Validators.minLength(4)]],
+            'email': ['', [Validators.required, Validators.email]],
+            'password': ['', [Validators.required, Validators.minLength(8)]],
             'birthDate': ['', [Validators.required]],
             'gender':['', [Validators.required]]
         });
+        this.userRegisterForm.valueChanges
+          .subscribe(data => this.onRegisterValueChanged(data));
     }
 
     login() {
@@ -59,6 +66,11 @@ export class UserLoginComponent implements OnInit {
     }
     
     register() {
+        let id = 'Agd2vhl';
+        let userProfileLink = this.userRegisterForm.value.name;
+        // console.log(userProfileLink);
+        console.log(this.userRegisterForm.errors);
+
         if (this.validateNewUserDetail()) {    
             let userData = JSON.stringify(this.userRegisterForm.value);
             this._userService.create(userData)
@@ -76,6 +88,55 @@ export class UserLoginComponent implements OnInit {
                 });
         }
     }
+
+    onRegisterValueChanged(data?: any) {
+        const form = this.userRegisterForm;
+        for (const field in this.registerFormErrors) {
+            // clear previous error message (if any)
+            this.registerFormErrors[field] = '';
+            const control = form.get(field);
+            if (control && control.dirty && !control.valid) {
+                const messages = this.registerValidationMessages[field];
+                for (const key in control.errors) {
+                    this.registerFormErrors[field] += messages[key] + ' ';
+                }
+            }
+        }
+    }
+    
+    registerFormErrors = {
+        'name': '',
+        'userName': '',
+        'email': '',
+        'password':'',
+        'birthDate':'',
+        'gender':''
+    };
+
+    registerValidationMessages = {
+        'name': {
+            'required':  'Name is required.',
+            'minlength': 'Name must be at least 4 characters long.',
+            'maxlength': 'Name cannot be more than 24 characters long.',
+        },
+        'userName': {
+            'required': 'Username is required.'
+        },
+        'email': {
+            'required': 'Email is required.',
+            'email':    'Please enter valid email address.'
+        },
+        'password': {
+            'required': 'Password is required.',
+            'minlength': 'Password must be at least 8 characters long.'
+        },
+        'birthDate': {
+            'required': 'Birth Date is required.'
+        },
+        'gender': {
+            'required': 'Gender is required.'
+        },
+    };
 
     validateUserCredentials(): boolean {
         if (this.userLoginForm.value.userName != '' && this.userLoginForm.value.password != '') {
@@ -111,6 +172,11 @@ export class UserLoginComponent implements OnInit {
         this.loginDivBox = loginDivBox;
         this.registerDivBox = registerDivBox;
     }
-    ngOnInit() {
+
+    public NoWhitespaceValidator(control: FormControl) {
+        let isWhitespace = (control.value || '').trim().length === 0;
+        let isValid = !isWhitespace;
+        return isValid ? null : { 'whitespace': true }
     }
+
 }
