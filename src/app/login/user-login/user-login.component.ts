c4a0695214c0bb4e33cc3eb605e0028450de0c26
karan95 <kmthakor95@gmail.com>
2017-07-29 19:31:55 +0530

    checkbox in form and validation message implementation

--- src/app/login/user-login/user-login.component.ts
+++ src/app/login/user-login/user-login.component.ts
@@ -4,7 +4,6 @@ import { Router, ActivatedRoute } from '@angular/router';
 import { MdSnackBar, MdSnackBarConfig } from '@angular/material';
 import { UserService } from '../../services/user/user.service';
 import { AuthenticationService } from '../../services/user/authentication.service';
-import { AlertService } from '../../lt-components/alert/alert.service';
 import { NotificationService } from '../../services/notification/notification.service';
 
 @Component({
@@ -15,8 +14,6 @@ import { NotificationService } from '../../services/notification/notification.se
 export class UserLoginComponent implements OnInit {
     userLoginForm: FormGroup;
     userRegisterForm: FormGroup;
-    loginDivBox = false;
-    registerDivBox = false;
 
     constructor(
         private route: ActivatedRoute,
@@ -24,7 +21,6 @@ export class UserLoginComponent implements OnInit {
         private formBuilder: FormBuilder,
         private _authenticationService: AuthenticationService,
         private _userService: UserService,
-        private _alertService: AlertService,
         private _notificationService: NotificationService
     ) {}
 
@@ -43,14 +39,15 @@ export class UserLoginComponent implements OnInit {
             'email': ['', [Validators.required, Validators.email]],
             'password': ['', [Validators.required, Validators.minLength(8)]],
             'birthDate': ['', [Validators.required]],
-            'gender':['', [Validators.required]]
+            'gender':['', [Validators.required]],
+            'ltTerms':[false, [Validators.required]]
         });
         this.userRegisterForm.valueChanges
           .subscribe(data => this.onRegisterValueChanged(data));
     }
 
     login() {
-        if (this.validateUserCredentials()) {
+        if (this.userLoginForm.valid) {
             let userLoginData = JSON.stringify(this.userLoginForm.value);
             this._authenticationService.login(userLoginData)
             .subscribe(
@@ -58,38 +55,36 @@ export class UserLoginComponent implements OnInit {
                     this.router.navigateByUrl('/');
                 },
                 error => {
-                    // this.loginDivBox = true;
-                    // this._alertService.error('The username or password is incorrect.');
                     this._notificationService.showErrorNotification('The username or password is incorrect.');
             });
+        } else {
+            this._notificationService.showErrorNotification('Please enter both username and password.');   
         }
     }
     
     register() {
+        this.onRegisterValueChanged();
         let id = 'Agd2vhl';
         let userProfileLink = this.userRegisterForm.value.name;
-        // console.log(userProfileLink);
-        console.log(this.userRegisterForm.errors);
 
-        if (this.validateNewUserDetail()) {    
+        if (this.userRegisterForm.valid) {  
             let userData = JSON.stringify(this.userRegisterForm.value);
             this._userService.create(userData)
             .subscribe(
                 data => {
                     this.userRegisterForm.reset();
-                    // this.alertPopupCheck(false, true);
-                    // this._alertService.success('Registration successful', true);
                     this._notificationService.showSuccessNotification('User Registration successful');
                 },
                 error => {
-                    // this.alertPopupCheck(false, true);
-                    // this._alertService.error('User registration unsuccessful.');
                     this._notificationService.showErrorNotification('User registration unsuccessful.');
                 });
+        } else {
+            this._notificationService.showErrorNotification('Please enter all details.'); 
         }
     }
 
     onRegisterValueChanged(data?: any) {
+        if (!this.userRegisterForm) { return; }
         const form = this.userRegisterForm;
         for (const field in this.registerFormErrors) {
             // clear previous error message (if any)
@@ -110,7 +105,8 @@ export class UserLoginComponent implements OnInit {
         'email': '',
         'password':'',
         'birthDate':'',
-        'gender':''
+        'gender':'',
+        'ltTerms':''
     };
 
     registerValidationMessages = {
@@ -120,7 +116,8 @@ export class UserLoginComponent implements OnInit {
             'maxlength': 'Name cannot be more than 24 characters long.',
         },
         'userName': {
-            'required': 'Username is required.'
+            'required': 'Username is required.',
+            'minlength': 'Username must be at least 4 characters long.'
         },
         'email': {
             'required': 'Email is required.',
@@ -136,42 +133,10 @@ export class UserLoginComponent implements OnInit {
         'gender': {
             'required': 'Gender is required.'
         },
+        'ltTerms': {
+            'required': 'Please check terms and conditions.'   
+        }        
     };
-
-    validateUserCredentials(): boolean {
-        if (this.userLoginForm.value.userName != '' && this.userLoginForm.value.password != '') {
-            return true;
-        } else {
-            this._notificationService.showErrorNotification('Please enter both username and password.');
-            // this.alertPopupCheck(true, false);
-            // this._alertService.error('Please enter both username and password.');
-            return false;
-        }
-    }
-
-    validateNewUserDetail(): boolean {
-        if (this.userRegisterForm.value.name != '' && this.userRegisterForm.value.userName != '' && this.userRegisterForm.value.email != ''
-            && this.userRegisterForm.value.password != '' && this.userRegisterForm.value.birthDate != '' && this.userRegisterForm.value.gender != '') {
-            if ((<HTMLInputElement>document.getElementById("registerRule")).checked) {
-                return true;
-            } else {
-                // this.alertPopupCheck(false, true);
-                // this._alertService.error('Please check terms and conditions.');
-                this._notificationService.showWarningNotification('Please check terms and conditions.');
-                return false;
-            }
-        } else {
-            // this.alertPopupCheck(false, true);
-            // this._alertService.error('Please enter all details.');
-            this._notificationService.showErrorNotification('Please enter all details.');
-            return false;
-        }
-    }
-
-    alertPopupCheck(loginDivBox, registerDivBox) {
-        this.loginDivBox = loginDivBox;
-        this.registerDivBox = registerDivBox;
-    }
 
     public NoWhitespaceValidator(control: FormControl) {
         let isWhitespace = (control.value || '').trim().length === 0;
